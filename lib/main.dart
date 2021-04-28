883e8c0f2bafb7ec43d6d8cb574ce6bbb952f21b
martcbit <martins.allysson@hotmail.com>
2021-04-27 23:46:59 -0300

    est-1

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List _toDoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+  Future<File> _saveData() async {
+
+  }
+
+}
+
+
+
+
